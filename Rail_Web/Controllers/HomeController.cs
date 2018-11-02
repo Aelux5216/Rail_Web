81f99b75caeef477e4dd3b17b740a430d1a59451
Aelux5216 <thedragongamers@gmail.com>
2018-11-02 01:13:55 +0000

    Initial Sync

---
+++ Rail_Web/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net;
+using Rail_Web.Models;
+
+namespace Rail_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["stations"] = getStations();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public List<string> getStations()
+        {
+            List<string> stations = new List<string>();
+
+            //Download latest stations
+
+            WebClient client = new System.Net.WebClient();
+
+            Uri url = new Uri("http://www.nationalrail.co.uk/static/documents/content/station_codes.csv", UriKind.Absolute);
+
+            try
+            {
+                client.DownloadFile(url, "StationList.csv");
+            }
+
+            catch
+            {
+
+            }
+            
+            //Setup reader for file
+            StreamReader reader = null;
+            List<string> stationList = new List<string>();
+            bool fileExists = false;
+
+            //Check if the txt file exists before trying to use
+            try
+            {
+                reader = new StreamReader("StationList.csv");
+                fileExists = true;
+            }
+            //If file doesn't exist try other dfs clusters.
+            catch
+            {
+
+            }
+
+            if (fileExists == true)
+            {
+                string output;
+
+                reader.ReadLine();
+
+                while ((output = reader.ReadLine()) != null)
+                {
+                    string output2 = output.Split(',')[0] + " (" + output.Split(',')[1] + ")";
+                    stationList.Add(output2);
+                }
+            }
+
+            return stationList;
+        }
+    }
+}
