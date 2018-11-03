aa3095af3a7fb8b97eb136f4d051a9645537805d
Aelux5216 <thedragongamers@gmail.com>
2018-11-03 23:51:20 +0000

    Middleware connected but unable to pass from controller to view.

--- Rail_Web/wwwroot/js/site.js
+++ Rail_Web/wwwroot/js/site.js
@@ -2,9 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function getSelection(eStat) {
-    var e = document.getElementById(eStat); //Get Id and newly selected quantity.
-    document.getElementById(eStat).options[document.getElementById(eStat).selectedIndex].value;
-
-    return eSelected;
-}
