fec874b247ceeb9925d2813f95432e73c4e472ba
Jason Stanford <jason.stanford@students.plymouth.ac.uk>
2018-11-13 22:47:10 +0000

    Function to recieve a serialized object from middleware using command.

--- Rail_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Rail_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -15,14 +15,14 @@ namespace Rail_Web.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class RegisterModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
 
         public RegisterModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender)
         {
@@ -128,7 +128,8 @@ namespace Rail_Web.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Username, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName
+                    ,PhoneNumber = Input.Phone,HouseName = Input.HouseName, Address1 = Input.Address1, Address2 = Input.Address2, Postcode = Input.Postcode };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
--- Rail_Web/Startup.cs
+++ Rail_Web/Startup.cs
@@ -56,7 +56,7 @@ namespace Rail_Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}/{id2?}");
             });
         }
     }
