5bd4642d1719c46b961843ccbadf989f3466afa3
Aelux5216 <thedragongamers@gmail.com>
2018-11-15 10:21:16 +0000

    Returned json converting to .net class now.

---
+++ Rail_Web/Models/CallingPoints.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rail_Web.Models
+{
+    public class CallingPoints
+    {
+        [JsonProperty("name")]
+        string name { get; set; }
+        [JsonProperty("code")]
+        string code { get; set; }
+        [JsonProperty("time")]
+        string time { get; set; }
+        [JsonProperty("status")]
+        string status { get; set; }
+    }
+}
