19c945ff1113335618d707f90baae6f1d3fbc34f
Jason Stanford <jason.stanford@students.plymouth.ac.uk>
2019-01-08 01:02:27 +0000

    Test for port

--- Rail_Web/Program.cs
+++ Rail_Web/Program.cs
@@ -19,6 +19,7 @@ namespace Rail_Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseSetting("https_port", "443")
                 .UseStartup<Startup>();
     }
 }
